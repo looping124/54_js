1acb36782e44f670c79b7e070bb0ed3ad48d5821
Cedric Le Net <lenetcedric@gmail.com>
2021-11-18 16:06:16 +0100

    script6

---
+++ script_6.js
@@ -0,0 +1,73 @@
+let btnScript_6 = document.querySelector('.script-6');
+let btnAns_6 = document.querySelector('.script-6-answer');
+
+btnScript_6.addEventListener('click', e => {
+  let arn = document.getElementById("arn").value;
+  arn = arn.replace(/\s/g, ''); //on efface les espaces
+  let arn_array = arn.match(/.{1,3}/g);
+  let index = 0;
+  let acides_array = [];
+  let arn_array_errors = 0;
+  arn_array.forEach(codon  => {
+    switch (codon) {
+      case 'UCU':
+      case 'UCC':
+      case 'UCA':
+      case 'UCG':
+      case 'AGU':
+      case 'AGC':
+        acides_array[index] = "Sérine";
+        // console.log(acides_array[index]);
+        index++;
+        break;
+      case 'CCU':
+      case 'CCC':
+      case 'CCA':
+      case 'CCG':
+        acides_array[index] = "Proline";
+        // console.log(acides_array[index]);
+        index++;
+        break;
+      case 'UUA':
+      case 'UUG':
+        acides_array[index] = "Leucine";
+        // console.log(acides_array[index]);
+        index++;
+        break;
+      case 'UUU':
+      case 'UUC':
+        acides_array[index] = "Phénylalanine";
+        // console.log(acides_array[index]);
+        index++;
+        break;
+      case 'CGU':
+      case 'CGC':
+      case 'CGA':
+      case 'CGG':
+      case 'AGA':
+      case 'AGG':
+        acides_array[index] = "Arginine";
+        // console.log(acides_array[index]);
+        index++;
+        break;
+      case 'UAU':
+      case 'UAC':
+        acides_array[index] = "Tyrosine";
+        // console.log(acides_array[index]);
+        index++;
+        break;
+      default:
+        arn_array_errors = 1;
+        break;
+    }
+  })
+    if(acides_array && arn_array_errors === 0) {
+      console.log(`${acides_array}`);
+      btnAns_6.innerText = `${acides_array}`;
+  } else if(arn_array_errors === 1 ) {
+      console.log(`Le code ARN n'est pas valide`);
+      btnAns_6.innerText = `Le code ARN n'est pas valide`;
+  }
+});
+
+//CCGUCGUUGCGCUACAGC

6a9ee28c1cbe7e358e99d21b78594883b410ff1d
looping124 <91526090+looping124@users.noreply.github.com>
2021-11-19 09:37:18 +0100

    Update script_1.js

--- script_1.js
+++ script_1.js
@@ -2,6 +2,7 @@ let btnScript_1 = document.querySelector('.script-1');
 let btnAns_1 = document.querySelector('.script-1-answer');
 
 btnScript_1.addEventListener('click', e => {
+  console.log(`Bonjour monde !`);
   let userName = document.getElementById("name_hello").value;
   if(userName) {
     console.log(`Bonjour ${userName} !`);
